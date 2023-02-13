5afa6e7b1531a315ba7dbd1d309ef2db938323af
SaraEs <alumno.417374@ies-azarquiel.es>
2023-02-13 17:00:39 +0100

    Hasta 1:55:33 Mostrar el listado en el html

--- src/main/resources/static/js/usuarios.js
+++ src/main/resources/static/js/usuarios.js
@@ -6,7 +6,7 @@ $(document).ready(function() {
 
 
 async function cargarUsuarios(){
-  const request = await fetch('usuarios}', {
+  const request = await fetch('usuarios', {
     method: 'GET',
     headers: {
       'Accept': 'application/json',
@@ -16,6 +16,10 @@ async function cargarUsuarios(){
   const usuarios = await request.json();
 
   console.log(usuarios);
+  let usuario = '<tr><td>123</td><td>Prueba Moy</td><td>lucasmoy@hotmail.com</td><td>632147895</td><td><a href="#" class="btn btn-danger btn-circle btn-sm"><i class="fas fa-trash"></i></a></td></tr>';
+
+  document.querySelector('#usuarios tbody').outerHTML = usuario;
+
 
 
 }
