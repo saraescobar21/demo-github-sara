50591596fa44b1fdceb464d630e46eeaa344146f
SaraEs <alumno.417374@ies-azarquiel.es>
2023-02-13 13:59:00 +0100

    Hasta 1.23.20 Creamos un paquete models y una clase usuario Creamos variables con getter and setter en Usuario. En el controller creamos una funcion que devuelva json de un usuario. Creamos funciones para modificar, eliminar y buscar usuarios.

--- src/main/java/com/s2daw/demo/controllers/UsuarioController.java
+++ src/main/java/com/s2daw/demo/controllers/UsuarioController.java
@@ -1,5 +1,6 @@
 package com.s2daw.demo.controllers;
 
+import com.s2daw.demo.models.Usuario;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,8 +9,43 @@ import java.util.List;
 @RestController
 public class UsuarioController {
 
-    @RequestMapping(value="prueba")
-    public List<String> prueba(){
-        return List.of("prueba","prueba2","prueba3");
+    @RequestMapping(value="usuario")
+    public Usuario getUsuario(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellidos("Moy");
+        usuario.setEmail("lucasmoy@hotmail.com");
+        usuario.setTelefono("632147895");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario")
+    public Usuario editar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellidos("Moy");
+        usuario.setEmail("lucasmoy@hotmail.com");
+        usuario.setTelefono("632147895");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario")
+    public Usuario eliminar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellidos("Moy");
+        usuario.setEmail("lucasmoy@hotmail.com");
+        usuario.setTelefono("632147895");
+        return usuario;
+    }
+
+    @RequestMapping(value="usuario")
+    public Usuario buscar(){
+        Usuario usuario = new Usuario();
+        usuario.setNombre("Lucas");
+        usuario.setApellidos("Moy");
+        usuario.setEmail("lucasmoy@hotmail.com");
+        usuario.setTelefono("632147895");
+        return usuario;
     }
 }
